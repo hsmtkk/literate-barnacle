527989d7a5400238ef870f7c74bab243d1f896a0
hsmtkk <hsmtkk@gmail.com>
2023-04-28 15:25:07 +0900

    if clause should be inside the form statement

--- front/front.py
+++ front/front.py
@@ -26,8 +26,8 @@ def show_bookings_page() -> None:
     booking_service = os.environ["BOOKING"]
 
     with st.form(key="booking"):
-        user_id: int = st.number_input("")
-        room_id: int = st.number_input("")
+        user_id: int = 1  # dummy
+        room_id: int = 2  # dummy
         reserved_num: int = st.number_input("予約人数", step=1)
         date: datetime.date = st.date_input("日付", min_value=datetime.date.today())
         start_time = st.time_input("開始時刻", value=datetime.time(hour=9, minute=0))
@@ -49,21 +49,21 @@ def show_bookings_page() -> None:
             minute=end_time.minute,
         ).isoformat()
 
-    if submit_button:
-        with grpc.insecure_channel(booking_service) as channel:
-            stub = booking_pb2_grpc.BookingServiceStub(channel)
-            response = stub.Create(
-                booking_pb2.CreateReqeust(
-                    booking=booking_pb2.NewBooking(
-                        user_id=user_id,
-                        room_id=room_id,
-                        reserved_num=reserved_num,
-                        start_date_time=start_date_time,
-                        end_date_time=end_date_time,
+        if submit_button:
+            with grpc.insecure_channel(booking_service) as channel:
+                stub = booking_pb2_grpc.BookingServiceStub(channel)
+                response = stub.Create(
+                    booking_pb2.CreateReqeust(
+                        booking=booking_pb2.NewBooking(
+                            user_id=user_id,
+                            room_id=room_id,
+                            reserved_num=reserved_num,
+                            start_date_time=start_date_time,
+                            end_date_time=end_date_time,
+                        )
                     )
                 )
-            )
-        st.write(response)
+            st.write(response)
 
 
 def show_rooms_page() -> None:
@@ -74,15 +74,15 @@ def show_rooms_page() -> None:
         capacity: int = st.number_input("定員", step=1)
         submit_button = st.form_submit_button(label="送信")
 
-    if submit_button:
-        with grpc.insecure_channel(room_service) as channel:
-            stub = room_pb2_grpc.RoomServiceStub(channel)
-            response = stub.Create(
-                room_pb2.CreateReqeust(
-                    room=room_pb2.NewRoom(name=name, capacity=capacity)
+        if submit_button:
+            with grpc.insecure_channel(room_service) as channel:
+                stub = room_pb2_grpc.RoomServiceStub(channel)
+                response = stub.Create(
+                    room_pb2.CreateReqeust(
+                        room=room_pb2.NewRoom(name=name, capacity=capacity)
+                    )
                 )
-            )
-        st.write(response)
+            st.write(response)
 
 
 def show_users_page() -> None:
@@ -92,13 +92,13 @@ def show_users_page() -> None:
         name: str = st.text_input("ユーザー名", max_chars=12)
         submit_button = st.form_submit_button(label="送信")
 
-    if submit_button:
-        with grpc.insecure_channel(user_service) as channel:
-            stub = user_pb2_grpc.UserServiceStub(channel)
-            response = stub.Create(
-                user_pb2.CreateReqeust(user=user_pb2.NewUser(name=name))
-            )
-        st.write(response)
+        if submit_button:
+            with grpc.insecure_channel(user_service) as channel:
+                stub = user_pb2_grpc.UserServiceStub(channel)
+                response = stub.Create(
+                    user_pb2.CreateReqeust(user=user_pb2.NewUser(name=name))
+                )
+            st.write(response)
 
 
 run()
